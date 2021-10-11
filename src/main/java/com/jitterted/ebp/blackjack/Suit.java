bc2466e1fee7702165cfed8eadbd7347b6184165
Ted M. Young <tedyoung@gmail.com>
2021-10-11 15:50:08 -0700

    Lab #3: Compiles and tests pass, but we're not done.

---
+++ src/main/java/com/jitterted/ebp/blackjack/Suit.java
@@ -0,0 +1,32 @@
+package com.jitterted.ebp.blackjack;
+
+import java.util.List;
+
+public enum Suit {
+    HEARTS("♥"),
+    CLUBS("♣"),
+    DIAMONDS("♦"),
+    SPADES("♠");
+
+    static final List<String> SUITS = List.of("♠", "♦", "♥", "♣");
+    private final String symbol;
+
+    Suit(String symbol) {
+        this.symbol = symbol;
+    }
+
+    // TEMPORARY
+    // SCAFFOLDING - will go away
+    public static Suit from(String symbol) {
+        for (Suit suit : values()) {
+            if (suit.symbol().equals(symbol)) {
+                return suit;
+            }
+        }
+        return null;
+    }
+
+    public String symbol() {
+        return symbol;
+    }
+}
