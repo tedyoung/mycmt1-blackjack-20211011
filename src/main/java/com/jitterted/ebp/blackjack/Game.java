0282251939c749d6dc4d5a7d387117aa11fafc6e
Ted M. Young <tedyoung@gmail.com>
2021-10-12 16:23:38 -0700

    Player betting within the Game, playerWins scenario works.

--- src/main/java/com/jitterted/ebp/blackjack/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/Game.java
@@ -12,6 +12,8 @@ public class Game {
 
     private final Hand dealerHand = new Hand();
     private final Hand playerHand = new Hand();
+    private int playerBalance = 0;
+    private int playerBetAmount = 0;
 
     public static void main(String[] args) {
         displayWelcomeScreen();
@@ -67,16 +69,21 @@ public class Game {
     private void determineOutcome(boolean playerBusted) {
         if (playerBusted) {
             System.out.println("You Busted, so you lose.  💸");
+            // playerLoses()
         } else if (dealerHand.isBusted()) {
             System.out.println("Dealer went BUST, Player wins! Yay for you!! 💵");
+            // playerWins()
         } else if (playerHand.beats(dealerHand)) { // playerHand.beats(dealerHand)
             System.out.println("You beat the Dealer! 💵");
+            // playerWins()
         } else if (dealerHand.pushes(playerHand)) {
             // "technical": compare, equals, matches
             // "domain": tie, push
             System.out.println("Push: You tie with the Dealer. 💸");
+            // playerPushes()
         } else {
             System.out.println("You lost to the Dealer. 💸");
+            // playerLoses()
         }
     }
 
@@ -167,5 +174,23 @@ public class Game {
         System.out.println(" (" + dealerHand.value() + ")");
 
         displayPlayerHand();
+    }
+
+
+    public int playerBalance() {
+        return playerBalance;
+    }
+
+    public void playerDeposits(int amount) {
+        playerBalance += amount;
+    }
+
+    public void playerBets(int betAmount) {
+        playerBalance -= betAmount;
+        playerBetAmount = betAmount;
+    }
+
+    public void playerWins() {
+        playerBalance += playerBetAmount * 2;
     }
 }
